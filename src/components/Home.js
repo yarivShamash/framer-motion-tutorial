0c6b787e8633cd0a6aca53b9202dc82968ffff09
Yariv Shamash <yarivs@webiya.co.il>
2021-06-20 14:01:14 +0300

    H2 cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
-import AnimatedH2 from "./AnimatedH2";
 import AnimatedButton from "./AnimatedButton";
 import { buttonVariants } from "../config/animations";
 
 const Home = () => {
-  const [showTitle, setShowTitle] = useState(true);
   const containerInitial = { opacity: 0 };
   const containerAnimation = { opacity: 1 };
   const containerTransition = { delay: 0.3, duration: 1.5 };
@@ -19,13 +17,9 @@ const Home = () => {
       animate={containerAnimation}
       transition={containerTransition}
     >
-      <AnimatePresence>
-        {showTitle && <AnimatedH2>Welcome to Pizza Joint</AnimatedH2>}
-      </AnimatePresence>
-      <button onClick={() => setShowTitle(!showTitle)}>
-        Fly title flyyyyy!
-      </button>
-      <Link onClick={() => setShowTitle(!showTitle)} to="/base">
+      <h2>Welcome to Pizza Joint</h2>
+
+      <Link to="/base">
         <AnimatedButton animationVariants={buttonVariants}>
           Create Your Pizza
         </AnimatedButton>
