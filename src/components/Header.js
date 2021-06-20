bd267a1a65cdfc6219f1edb9ada729d71a4d5383
Yariv Shamash <yarivs@webiya.co.il>
2021-06-20 15:48:44 +0300

    svg animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { svgVariants, pathVariants } from "../config/animations";
 
 const Header = () => {
   const titleInitial = { y: -250 };
@@ -8,17 +9,29 @@ const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg
+        <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
+          variants={svgVariants}
+          initial="initial"
+          animate="visible"
         >
-          <path
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
+            initial="initial"
+            animate="visible"
           />
-          <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-        </svg>
+          <motion.path
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariants}
+            initial="initial"
+            animate="visible"
+          />
+        </motion.svg>
       </div>
       <motion.div
         className="title"
--- src/config/animations.js
+++ src/config/animations.js
@@ -1,3 +1,17 @@
+export const svgVariants = {
+  initial: { rotate: -180 },
+  visible: { rotate: 0, transition: { duration: 1 } },
+};
+
+export const pathVariants = {
+  initial: { opacity: 0, pathLength: 0 },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { duration: 2, ease: "easeInOut" },
+  },
+};
+
 export const homeContainerVariants = {
   hidden: { opacity: 0 },
   visible: { opacity: 1, transition: { delay: 0.3, duration: 1.5 } },
